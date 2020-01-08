0153d3bc0f902e9576ba503171ee9ca6cfbd86aa
shilin <shilin@csmail.citicbank.com>
2020-01-08 21:26:51 +0800

    fix

--- src/cookieShim.js
+++ src/cookieShim.js
@@ -28,7 +28,8 @@ const cookieStore = (function () {
       if (util.isAlipay()) {
         // 请求时带上设置的 cookies
         options.headers = options.headers || {}
-        options.headers['Cookie'] = requestCookies
+        //支付宝不处理cookie，由系统自己处理
+        // options.headers['Cookie'] = requestCookies
         options.headers['X-Requested-With'] = 'XMLHttpRequest'
         if (options.dataType === 'json') {
           options.headers['Accept'] = 'application/json, text/plain, */*'
@@ -49,7 +50,8 @@ const cookieStore = (function () {
         // 获取响应 cookies
         let responseCookies
         if (util.isAlipay()) {
-          responseCookies = response.headers ? response.headers['Set-Cookie'] || response.headers['set-cookie'] : ''
+          //支付宝不处理cookie，由系统自己处理
+          // responseCookies = response.headers ? response.headers['Set-Cookie'] || response.headers['set-cookie'] : ''
         } else {
           responseCookies = response.header ? response.header['Set-Cookie'] || response.header['set-cookie'] : ''
         }
